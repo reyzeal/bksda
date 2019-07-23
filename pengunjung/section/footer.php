cda687f8034909514617836f407df01a73139f54
Rizal Ardhi Rahmadani <rizal.ardhi.rahmadani@gmail.com>
2019-07-23 14:26:23 +0700

    done

--- pengunjung/section/footer.php
+++ pengunjung/section/footer.php
@@ -43,7 +43,7 @@
                 <div class="form">
                     <div id="sendmessage">Your message has been sent. Thank you!</div>
                     <div id="errormessage"></div>
-                    <form action="/pengunjung/proses/feedback.php" method="post" role="form" class="contactForm">
+                    <form action="../pengunjung/proses/feedback.php" method="post" role="form" class="contactForm">
                         <div class="form-row">
                             <div class="form-group col-lg-6">
                                 <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
